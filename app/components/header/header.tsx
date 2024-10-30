1615d954bd8b6af36d6cc010d016d14b2be1eddb
Artem <68074370+SedifBuster@users.noreply.github.com>
2024-10-30 16:11:35 +0300

    zyn yan first

---
+++ app/components/header/header.tsx
@@ -0,0 +1,15 @@
+
+
+export
+  function Header() {
+    return <header className="flex gap-6">
+        IMAGE
+        <nav className="flex gap-4">
+            <p>menu</p>
+            <p>gallery</p>
+            <p>about us</p>
+            <p>contact us</p>
+        </nav>
+
+    </header>
+  }
