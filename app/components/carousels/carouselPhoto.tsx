1ce2c8ce001553bc9ade6701555900833fd2fbdc
Artem <68074370+SedifBuster@users.noreply.github.com>
2024-11-08 16:48:15 +0300

    carousels update

--- app/components/carousels/carouselPhoto.tsx
+++ app/components/carousels/carouselPhoto.tsx
@@ -7,36 +7,22 @@ import {
   EmblaOptionsType
 } from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
-/*import {
-  NextButton,
-  PrevButton,
-  usePrevNextButtons
-} from './EmblaCarouselArrowButtons'
-import { DotButton, useDotButton } from './EmblaCarouselDotButton'*/
+import Image, { StaticImageData } from 'next/image'
+import Autoplay from 'embla-carousel-autoplay'
+
 
 const TWEEN_FACTOR_BASE = 0.2
 
 type PropType = {
-  slides: number[]
+  slides: { id: number, sourse: StaticImageData; altText: string; }[]
   options?: EmblaOptionsType
 }
 
 const CarouselPhoto: React.FC<PropType> = (props) => {
   const { slides, options } = props
-  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay({delay: 10000, jump: false})])
   const tweenFactor = useRef(0)
   const tweenNodes = useRef<HTMLElement[]>([])
-
- /* const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
-
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
-  */
 
   const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
     tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
@@ -107,14 +93,15 @@ const CarouselPhoto: React.FC<PropType> = (props) => {
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
+          {slides.map((slide) => (
+            <div className="embla__slide" key={slide.id}>
               <div className="embla__parallax">
                 <div className="embla__parallax__layer">
-                  <img
+                  <Image
                     className="embla__slide__img embla__parallax__img"
-                    src={`https://picsum.photos/600/350?v=${index}`}
-                    alt="Your alt text"
+                    src={slide.sourse}
+                    alt={slide.altText}
+                    width={1280}
                   />
                 </div>
               </div>
@@ -137,147 +124,3 @@ const CarouselPhoto: React.FC<PropType> = (props) => {
 }
 
 export default CarouselPhoto
-
-
-/**  
- *           <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
- * 
- *        {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
-              )}
-            />
-          ))}
-            
-          .embla {
-  max-width: 48rem;
-  margin: auto;
-  --slide-height: 19rem;
-  --slide-spacing: 1rem;
-  --slide-size: 80%;
-}
-.embla__viewport {
-  overflow: hidden;
-}
-.embla__container {
-  display: flex;
-  touch-action: pan-y pinch-zoom;
-  margin-left: calc(var(--slide-spacing) * -1);
-}
-.embla__slide {
-  transform: translate3d(0, 0, 0);
-  flex: 0 0 var(--slide-size);
-  min-width: 0;
-  padding-left: var(--slide-spacing);
-}
-.embla__slide__img {
-  border-radius: 1.8rem;
-  display: block;
-  height: var(--slide-height);
-  width: 100%;
-  object-fit: cover;
-}
-.embla__controls {
-  display: grid;
-  grid-template-columns: auto 1fr;
-  justify-content: space-between;
-  gap: 1.2rem;
-  margin-top: 1.8rem;
-}
-.embla__buttons {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 0.6rem;
-  align-items: center;
-}
-.embla__button {
-  -webkit-tap-highlight-color: rgba(var(--text-high-contrast-rgb-value), 0.5);
-  -webkit-appearance: none;
-  appearance: none;
-  background-color: transparent;
-  touch-action: manipulation;
-  display: inline-flex;
-  text-decoration: none;
-  cursor: pointer;
-  border: 0;
-  padding: 0;
-  margin: 0;
-  box-shadow: inset 0 0 0 0.2rem var(--detail-medium-contrast);
-  width: 3.6rem;
-  height: 3.6rem;
-  z-index: 1;
-  border-radius: 50%;
-  color: var(--text-body);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.embla__button:disabled {
-  color: var(--detail-high-contrast);
-}
-.embla__button__svg {
-  width: 35%;
-  height: 35%;
-}
-.embla__dots {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-end;
-  align-items: center;
-  margin-right: calc((2.6rem - 1.4rem) / 2 * -1);
-}
-.embla__dot {
-  -webkit-tap-highlight-color: rgba(var(--text-high-contrast-rgb-value), 0.5);
-  -webkit-appearance: none;
-  appearance: none;
-  background-color: transparent;
-  touch-action: manipulation;
-  display: inline-flex;
-  text-decoration: none;
-  cursor: pointer;
-  border: 0;
-  padding: 0;
-  margin: 0;
-  width: 2.6rem;
-  height: 2.6rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-}
-.embla__dot:after {
-  box-shadow: inset 0 0 0 0.2rem var(--detail-medium-contrast);
-  width: 1.4rem;
-  height: 1.4rem;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  content: '';
-}
-.embla__dot--selected:after {
-  box-shadow: inset 0 0 0 0.2rem var(--text-body);
-}
-.embla__parallax {
-  border-radius: 1.8rem;
-  height: 100%;
-  overflow: hidden;
-}
-.embla__parallax__layer {
-  position: relative;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-.embla__parallax__img {
-  max-width: none;
-  flex: 0 0 calc(115% + (var(--slide-spacing) * 2));
-  object-fit: cover;
-}
-
-          
-          */
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,92 @@ import CarouselPhoto from "./components/carousels/carouselPhoto";
 import { EmblaOptionsType } from 'embla-carousel'
 import { Header } from "./components/header/header";
 import logoPic1 from "../public/logo1.webp"
+import CarouselHits from "./components/carousels/carouselHits";
 
-export default function Home() {
+//slidersPhoto
+import slide1 from "../public/carouselPh/1.webp"
+import slide2 from "../public/carouselPh/2.webp"
+import slide3 from "../public/carouselPh/3.webp"
+import slide4 from "../public/carouselPh/4.webp"
+import slide5 from "../public/carouselPh/5.webp"
+import slide6 from "../public/carouselPh/6.webp"
+import slide7 from "../public/carouselPh/7.webp"
+import slide8 from "../public/carouselPh/8.webp"
+import slide9 from "../public/carouselPh/9.webp"
+import slide10 from "../public/carouselPh/10.webp"
+import slide11 from "../public/carouselPh/11.webp"
+import CarouselDinners from "./components/carousels/carouselDinners";
 
-const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
-const SLIDE_COUNT = 5
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+const slidesPhoto = [
+  {
+    id: 1,
+    sourse: slide1,
+    altText: "Зун Ян - ресторан китайской кухни"
+  },
+  {
+    id: 2,
+    sourse: slide2,
+    altText: "принимаем заявки на Новогодние корпоративы"
+  },
+  {
+    id: 3,
+    sourse: slide3,
+    altText: "бизнес ланчи по будням. Китайская кухня по доступным ценам"
+  },
+  {
+    id: 4,
+    sourse: slide4,
+    altText: "живая музыка и танцпол - все для идеального вечера"
+  },
+  {
+    id: 5,
+    sourse: slide5,
+    altText: "фотография внутри ресторана"
+  },
+  {
+    id: 6,
+    sourse: slide6,
+    altText: "повара приготовят настоящие шедевры китайской кухни"
+  },
+  {
+    id: 7,
+    sourse: slide7,
+    altText: "фотография террасы"
+  },
+  {
+    id: 8,
+    sourse: slide8,
+    altText: "аутентичное меню с большими порциями, свежие морепродукты и пельмешки"
+  },
+  {
+    id: 9,
+    sourse: slide9,
+    altText: "место для проведений банкетов и мероприятий"
+  },
+  {
+    id: 10,
+    sourse: slide10,
+    altText: "уютная терраса, открытая в любое время года"
+  },
+  {
+    id: 11,
+    sourse: slide11,
+    altText: "Аквариум с живыми крабами"
+  },
+]
+
+export
+  default function Home(
+
+) {
+  const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true, duration: 10 }
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+
+
+
+
   return (
     <div className=" font-[family-name:var(--font-geist-sans)] ">
       <section className="bg-[url('../public/back.jpg')] bg-center bg-no-repeat bg-cover">
@@ -37,14 +117,14 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
       <section className="pb-12 pt-12" id="menuAnchor">
       <div className="flex flex-col gap-8 items-center container mx-auto">
        <h3 className="text-black w-full text-center text-5xl pt-8 font-semibold">ХИТЫ В НАШЕМ ЗАВЕДЕНИИ</h3>
-       <CarouselPhoto slides={SLIDES} options={OPTIONS}/>
+       <CarouselHits slides={SLIDES} />
+     
       </div>
       </section>
 
-      <section className=" h-[85vh] bg-[#413026]" id="about_usAnchor">
+      <section className="pt-6 pb-6 bg-[#413026]" id="about_usAnchor">
         <div className="flex flex-col gap-4 items-center sm:items-start container mx-auto">
-          <h3 className="text-white text-5xl font-bold w-full text-center">slider</h3>
-          <CarouselPhoto slides={SLIDES} options={OPTIONS}/>
+          <CarouselPhoto slides={slidesPhoto} options={OPTIONS}/>
         </div>
       </section>
 
@@ -57,7 +137,7 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 с 12:00 до 15:00
 
 за 350 рублей</h3>
-       <CarouselPhoto slides={SLIDES} options={OPTIONS}/>
+     <CarouselDinners slides={SLIDES}/>
       </div>
       </section>
 
