8a56dda6cd6fe2ec66ab1ed8fd90536362508342
Artem <68074370+SedifBuster@users.noreply.github.com>
2024-11-22 15:45:35 +0300

    2gis issue

---
+++ app/components/map/map.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { load } from '@2gis/mapgl';
+import React from 'react';
+import { useEffect } from 'react';
+//import MapWrapper from './mapWrapper';
+
+const MapWrapper = React.memo(
+    () => {
+        return <div id="map-container" style={{ width: '100%', height: '100%' }}></div>;
+    },
+    () => true,
+);
+
+export
+  default function Map() {
+
+    useEffect(() => {
+        let map: any;
+        load().then((mapglAPI) => {
+            map = new mapglAPI.Map('map-container', {
+                center: [43.11251294606876, 131.87597751617434],
+                zoom: 16,
+                key: '70000001084274180',
+            });
+        });
+        return () => map && map.destroy();
+    }, []);
+
+    return <div style={{ width: '100%', height: '100%' }}>
+        <MapWrapper />
+    </div>
+  }
